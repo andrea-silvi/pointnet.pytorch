aababca806d0e37d291f76e1fb6bc7999122689f
vitto <vittoriop.17@gmail.com>
2021-05-11 10:40:59 +0200

    train example

--- utils/train_ae.py
+++ utils/train_ae.py
@@ -72,6 +72,103 @@ def example_AE_and_chamfer_loss():
     loss = chamfer_loss(decoded, points)
     print(loss)
 
+def train_example(opt):
+    dataset = ShapeNetDataset(
+        root=opt.dataset,
+        class_choice="Knife",
+        npoints=opt.num_points)
+
+    test_dataset = ShapeNetDataset(
+        root=opt.dataset,
+        split='test',
+        class_choice="Knife",
+        npoints=opt.num_points,
+        data_augmentation=False)
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=opt.batchSize,
+        shuffle=True,
+        num_workers=int(opt.workers))
+
+    testdataloader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=opt.batchSize,
+        shuffle=True,
+        num_workers=int(opt.workers))
+
+    print(len(dataset), len(test_dataset))
+
+    try:
+        os.makedirs(opt.outf)
+    except OSError:
+        pass
+
+    autoencoder = PointNet_AutoEncoder(opt.num_points, opt.feature_transform)
+
+    # TODO - import pointnet parameters (encoder network)
+    if opt.model != '':
+        autoencoder.load_state_dict(torch.load(opt.model))
+
+    optimizer = optim.Adam(autoencoder.parameters(), lr=0.001, betas=(0.9, 0.999))
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+    autoencoder.cuda()
+
+    num_batch = len(dataset) / opt.batchSize
+    # TODO - modify number of epochs (from 5 to opt.nepoch)
+    for epoch in range(5):
+        scheduler.step()
+        for i, points in enumerate(dataloader, 0):
+            print(f"Points size: {points.size()}")
+            points = points.transpose(2, 1)
+            points = points.cuda()
+            optimizer.zero_grad()
+            autoencoder = autoencoder.train()
+            decoded_points = autoencoder(points)
+            print(f"Decoded points size: {decoded_points.size()}")
+            decoded_points = decoded_points.cuda()
+            chamfer_loss = PointLoss()  #  instantiate the loss
+            # let's compute the chamfer distance between the two sets: 'points' and 'decoded'
+            loss = chamfer_loss(decoded_points, points)
+            # if opt.feature_transform:
+            #     loss += feature_transform_regularizer(trans_feat) * 0.001
+            loss.backward()
+            optimizer.step()
+            print('[%d: %d/%d] train loss: %f' % (
+            epoch, i, num_batch, loss.item()))
+
+            # if i % 10 == 0:
+            #     j, data = next(enumerate(testdataloader, 0))
+            #     points, target = data
+            #     target = target[:, 0]
+            #     points = points.transpose(2, 1)
+            #     points, target = points.cuda(), target.cuda()
+            #     classifier = classifier.eval()
+            #     pred, _, _ = classifier(points)
+            #     loss = F.nll_loss(pred, target)
+            #     pred_choice = pred.data.max(1)[1]
+            #     correct = pred_choice.eq(target.data).cpu().sum()
+            #     print('[%d: %d/%d] %s loss: %f accuracy: %f' % (
+            #     epoch, i, num_batch, blue('test'), loss.item(), correct.item() / float(opt.batchSize)))
+
+        torch.save(autoencoder.state_dict(), '%s/cls_model_%d.pth' % (opt.outf, epoch))
+
+    # total_correct = 0
+    # total_testset = 0
+    # for i, data in tqdm(enumerate(testdataloader, 0)):
+    #     points, target = data
+    #     target = target[:, 0]
+    #     points = points.transpose(2, 1)
+    #     points, target = points.cuda(), target.cuda()
+    #     classifier = classifier.eval()
+    #     pred, _, _ = classifier(points)
+    #     pred_choice = pred.data.max(1)[1]
+    #     correct = pred_choice.eq(target.data).cpu().sum()
+    #     total_correct += correct.item()
+    #     total_testset += points.size()[0]
+    #
+    # print("final accuracy {}".format(total_correct / float(total_testset)))
+
 
 # TODO - Implement training phase (you should also implement cross-validation for tuning the hyperparameters)
 # TODO - You should also implement the visualization tools (visualization_tools package)
