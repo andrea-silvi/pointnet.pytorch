b0fe0bffb24bf6a0d4a9ed765593966b1cbeaefa
Andrea Silvi <andrea.silvi@studenti.polito.it>
2021-05-12 14:51:03 +0200

    loss plot fix

--- train_ae.py
+++ train_ae.py
@@ -189,7 +189,7 @@ def train_example(opt):
     plt.ylabel('loss')
     plt.legend(['Training', 'Validation'])
     plt.title('Loss vs. No. of epochs')
-
+    plt.show()
     # total_correct = 0
     # total_testset = 0
     # for i, data in tqdm(enumerate(testdataloader, 0)):
