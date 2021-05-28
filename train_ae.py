3fc61d49a5677f30e67da288e19886f6bf66b53a
fabio <fbtattix@gmail.com>
2021-05-28 11:30:04 +0200

    GridSearch

--- train_ae.py
+++ train_ae.py
@@ -10,7 +10,10 @@ import torch.optim as optim
 import torch.utils.data
 from utils.dataset import ShapeNetDataset
 from torch.utils.data import random_split
+import matplotlib
 import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+import gc
 
 # the following function doesn't make the training of the network!!
 # It shows the interfaces with the classes necessary for the point cloud completion task
@@ -55,19 +58,31 @@ def example_AE_and_chamfer_loss():
     print(loss)
 
 
+def print_loss_graph(training_history, val_history):
+    plt.plot(training_history, '-bx')
+    plt.plot(val_history, '-rx')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(['Training', 'Validation'])
+    plt.title('Loss vs. No. of epochs')
+    plt.savefig('loss.png', bbox_inches='tight',)
+
+
 def train_example(opt):
     random_seed = 43
     torch.manual_seed(random_seed)
+    #writer = SummaryWriter('runs/train_ae_experiment_1')
 
-    dataset = ShapeNetDataset(
+    training_dataset = ShapeNetDataset(
         root=opt.dataset,
         class_choice=opt.train_class_choice,
         npoints=opt.num_points)
 
-    val_size = int(0.2 * len(dataset))
-    train_size = len(dataset) - val_size
-
-    training_dataset, validation_dataset = random_split(dataset, [train_size, val_size])
+    validation_dataset = ShapeNetDataset(
+        root=opt.dataset,
+        split='val',
+        class_choice=opt.train_class_choice,
+        npoints=opt.num_points)
 
     test_dataset = ShapeNetDataset(
         root=opt.dataset,
@@ -88,13 +103,13 @@ def train_example(opt):
         shuffle=True,
         num_workers=int(opt.workers))
 
-    testdataloader = torch.utils.data.DataLoader(
+    test_dataloader = torch.utils.data.DataLoader(
          test_dataset,
          batch_size=opt.batchSize,
          shuffle=True,
          num_workers=int(opt.workers))
 
-    print(len(training_dataset), len(val_dataloader), len(test_dataset))
+    print(len(training_dataset), len(validation_dataset), len(test_dataset))
 
     try:
         os.makedirs(opt.outf)
@@ -111,15 +126,19 @@ def train_example(opt):
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
     autoencoder.cuda()
 
-    num_batch = len(dataset) / opt.batchSize
+
+    #num_batch = len(dataset) / opt.batchSize
     # TODO - modify number of epochs (from 5 to opt.nepoch)
 
     training_history = []
     val_history = []
+    gc.collect()
+    torch.cuda.empty_cache()
 
-    for epoch in range(8):
+    for epoch in range(10):
         scheduler.step()
         training_losses = []
+        #running_loss = 0.0
         for i, points in enumerate(train_dataloader, 0):
             # print(f"Points size: {points.size()}")
             # points = points.transpose(2, 1)
@@ -132,32 +151,36 @@ def train_example(opt):
             chamfer_loss = PointLoss()  #  instantiate the loss
             # let's compute the chamfer distance between the two sets: 'points' and 'decoded'
             loss = chamfer_loss(decoded_points, points)
+            training_losses.append(loss)
             # if opt.feature_transform:
             #     loss += feature_transform_regularizer(trans_feat) * 0.001
-            training_losses.append(loss)
             loss.backward()
+            #running_loss += loss.item()
             optimizer.step()
+            #if i % 1000 == 999: #every 1000 mini batches
+                #writer.add_scalar('training loss', running_loss/1000, epoch * len(train_dataloader))
 
-
+        gc.collect()
+        torch.cuda.empty_cache()
 
         # TODO - VALIDATION PHASE
-        val_losses = []
-        for j, val_points in enumerate(val_dataloader, 0):
-            autoencoder.eval()
-            val_points = val_points.cuda()
-            decoded_val_points = autoencoder(val_points)
-            decoded_val_points = decoded_val_points.cuda()
-            chamfer_loss = PointLoss()  #  instantiate the loss
-            val_loss = chamfer_loss(decoded_val_points, val_points)
-            val_losses.append(val_loss)
-        #training_losses = np.array(training_losses)
-        #val_losses = np.array(val_losses)
-        train_mean = torch.stack(training_losses).mean().item()
-        val_mean = torch.stack(val_losses).mean().item()
-        print(f' train loss:epoch {epoch} ,  {train_mean}')
-        print(f' validation loss:epoch {epoch} ,  {val_mean}')
-        training_history.append(train_mean)
-        val_history.append(val_mean)
+        with torch.no_grad():
+            val_losses = []
+            for j, val_points in enumerate(val_dataloader, 0):
+                autoencoder.eval()
+                val_points = val_points.cuda()
+                decoded_val_points = autoencoder(val_points)
+                decoded_val_points = decoded_val_points.cuda()
+                chamfer_loss = PointLoss()  #  instantiate the loss
+                val_loss = chamfer_loss(decoded_val_points, val_points)
+                val_losses.append(val_loss)
+
+            train_mean = torch.stack(training_losses).mean().item()
+            val_mean = torch.stack(val_losses).mean().item()
+            print(f'epoch: {epoch} , training loss: {train_mean}, validation loss: {val_mean}')
+
+            training_history.append(train_mean)
+            val_history.append(val_mean)
 
             # if i % 10 == 0:
             #     j, data = next(enumerate(testdataloader, 0))
@@ -177,14 +200,10 @@ def train_example(opt):
 
 
     # TODO PLOT LOSSES
-    plt.plot(training_losses, '-bx')
-    plt.plot(val_losses, '-rx')
-    plt.xlabel('epoch')
-    plt.ylabel('loss')
-    plt.legend(['Training', 'Validation'])
-    plt.title('Loss vs. No. of epochs')
-    plt.show();
-    plt.savefig('Loss vs. No. of epochs.png')
+    print(training_history)
+    print(val_history)
+    print_loss_graph(training_history, val_history)
+
     # total_correct = 0
     # total_testset = 0
     # for i, data in tqdm(enumerate(testdataloader, 0)):
@@ -207,8 +226,10 @@ if __name__=='__main__':
     # TODO - create a json for the FINAL arguments (after the cross-validation, e.g.: {'batchSize': 32})
     # TODO - and a json for the GRID SEARCH phase (e.g.: {'batchSize': [16, 32, 64], ...}
     parser = argparse.ArgumentParser()
+
+
     parser.add_argument('--batchSize', type=int, default=32, help='input batch size')
-    parser.add_argument('--num_points', type=int, default=2500, help='input batch size')
+    parser.add_argument('--num_points', type=int, default=1024, help='number points point cloud')
     parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
     parser.add_argument('--nepoch', type=int, default=250, help='number of epochs to train for')
     parser.add_argument('--outf', type=str, default='cls', help='output folder')
@@ -218,6 +239,9 @@ if __name__=='__main__':
     parser.add_argument('--test_class_choice', type=str, default="Knife", help="Test class")
     # parser.add_argument('--dataset_type', type=str, default='shapenet', help="dataset type shapenet|modelnet40")
     parser.add_argument('--feature_transform', action='store_true', help="use feature transform")
+    parser.add_argument('--scheduler-gamma', type=int, default=0.5, help="reduction factor of the learning rate")
+    parser.add_argument("--scheduler-stepSize", type= int, default=20)
+    parser.add_argument("--dropout", type=int, default=1, help="dropout percentage")
     opt = parser.parse_args()
     print(opt)
     train_example(opt)
