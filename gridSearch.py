063ba9d97ce46bf5421f524b989b8ffd6527deea
vitto <vittoriop.17@gmail.com>
2021-05-29 16:53:49 +0200

    fake test, fixed

--- gridSearch.py
+++ gridSearch.py
@@ -15,7 +15,7 @@ def fake_test(set_size=0.2):
             param_sets.append(command)
             param_sets.append(value)
         param_sets.append("--set_size")
-        param_sets.append(float(set_size))
+        param_sets.append(str(set_size))
         param_sets.append("--nepoch")
         param_sets.append(str(10))
         subprocess.run(["python", "train_ae.py"] + param_sets)
