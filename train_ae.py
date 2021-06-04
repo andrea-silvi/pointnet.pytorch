75f5c79a09e5fa63f9a4113dec67ef6a1614d5d7
fabio <fbtattix@gmail.com>
2021-06-04 11:26:04 +0200

    PrintPointCloud module added

--- train_ae.py
+++ train_ae.py
@@ -181,6 +181,7 @@ def train_example(opt):
                 autoencoder.eval()
                 val_points = val_points.cuda()
                 print(val_points.shape)
+                print(val_points[0,:,:])
 
                 decoded_val_points = autoencoder(val_points)
                 if (flag_stampa is False) and (epoch == n_epoch):
