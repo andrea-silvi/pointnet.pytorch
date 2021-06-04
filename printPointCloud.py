504946a3b13c9a0c78b27d9ebed9cc012ca6b683
fabio <fbtattix@gmail.com>
2021-06-04 11:44:31 +0200

    PrintPointCloud module added

--- printPointCloud.py
+++ printPointCloud.py
@@ -2,19 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Insert the cloud path in order to print it
-def printCloudFile(cloud):
+def printCloudFile(cloud, name):
         alpha = 0.5
         xyz = np.loadtxt(cloud)
         #print(xyz)
         fig = plt.figure(figsize=(15, 15))
         ax = fig.add_subplot(111, projection='3d')
         ax.plot(xyz[:, 0], xyz[:, 1], xyz[:, 2], 'o', alpha=alpha)
-        plt.show()
-def printCloud(cloud):
+        plt.savefig(f"{name}.png")
+def printCloud(cloud, name):
         alpha = 0.5
         xyz = cloud
         #print(xyz)
         fig = plt.figure(figsize=(15, 15))
         ax = fig.add_subplot(111, projection='3d')
         ax.plot(xyz[:, 0], xyz[:, 1], xyz[:, 2], 'o', alpha=alpha)
-        plt.show()
+        plt.savefig(f"{name}.png")
--- train_ae.py
+++ train_ae.py
@@ -189,7 +189,8 @@ def train_example(opt):
                     np.savetxt("validation_point", val_stamp, delimiter=" ")
                     np.savetxt("decoded_validation_point", dec_val_stamp, delimiter=" ")
                     flag_stampa=True
-                    ptPC.printCloud(val_stamp)
+                    ptPC.printCloud(val_stamp, "original_validation_points")
+                    ptPC.printCloud(decoded_val_points,"decoded_validation_points")
 
 
 
