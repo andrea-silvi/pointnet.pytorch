8fa02e6ac16d80f124326b45d58ac893b7e150da
fabio <fbtattix@gmail.com>
2021-06-06 10:57:16 +0200

    PrintPointCloud module added

--- gridSearch.py
+++ gridSearch.py
@@ -145,8 +145,9 @@ def optimize_params(filepath=os.path.join("parameters", "lr_params.json"), hyper
 
         point_cloud_np = point_cloud_np.cpu().numpy()
         dec_val_stamp = decoded_point_cloud.cpu().data.numpy()
-        ptPC.printCloud(point_cloud_np, "original_validation_points", args)
-        ptPC.printCloud(dec_val_stamp, "decoded_validation_points", args)
+        ptPC.printCloud(point_cloud_np, dec_val_stamp, "a", args)
+        #ptPC.printCloud(point_cloud_np, "original_validation_points", args)
+        #ptPC.printCloud(dec_val_stamp, "decoded_validation_points", args)
         dict_params[hash(str(args))] = str(args)
     folder = args.outf
     try:
