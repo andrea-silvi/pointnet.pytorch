8a0a816d098e25705d8505d051755e0c8761ce83
fabio <fbtattix@gmail.com>
2021-06-06 10:20:12 +0200

    PrintPointCloud module added

--- visualization_tools/printPointCloud.py
+++ visualization_tools/printPointCloud.py
@@ -4,13 +4,18 @@ import os
 
 
 # Insert the cloud path in order to print it
-def printCloudFile(cloud, name):
+def printCloudFile(cloud_original, cloud_decoded, name):
     alpha = 0.5
-    xyz = np.loadtxt(cloud)
+    xyz = np.loadtxt(cloud_original)
     # print(xyz)
     fig = plt.figure(figsize=(15, 15))
     ax = fig.add_subplot(111, projection='3d')
     ax.plot(xyz[:, 0], xyz[:, 1], xyz[:, 2], 'o', alpha=alpha)
+    ax.set_title("original cloud")
+    ax.append(fig.add_subplot(111,projection='3d'))
+
+    ax[-1].plot(cloud_decoded[:, 0], cloud_decoded[:, 1], cloud_decoded[:, 2], 'o', alpha=alpha)
+    ax[-1].set_title("decoded cloud")
     plt.savefig(f"/content/pointnet.pytorch/{name}.png")
 
 """
