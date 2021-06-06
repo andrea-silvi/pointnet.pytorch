b8568e493fccb128a5ea97a399641fcd880bcc62
fabio <fbtattix@gmail.com>
2021-06-06 11:04:43 +0200

    PrintPointCloud module added

--- visualization_tools/printPointCloud.py
+++ visualization_tools/printPointCloud.py
@@ -42,7 +42,7 @@ def printCloud(cloud_original, cloud_decoded, name, alpha=0.5, opt=None):
 def printCloud(cloud_original, cloud_decoded, name, alpha=0.5, opt=None):
     alpha = 0.5
     xyz = cloud_original[0]
-    # print("sono qui")
+    print("sono qui")
     fig = plt.figure(figsize=(15, 15))
     ax = fig.add_subplot(111, projection='3d')
     ax.plot(xyz[:, 0], xyz[:, 1], xyz[:, 2], 'o', alpha=alpha)
