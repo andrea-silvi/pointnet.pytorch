6298c2397c5c6fe5a7d6816e57b6318efd360ec0
fabio <fbtattix@gmail.com>
2021-06-06 11:10:45 +0200

    PrintPointCloud module added

--- visualization_tools/printPointCloud.py
+++ visualization_tools/printPointCloud.py
@@ -19,27 +19,23 @@ def printCloudFile(cloud_original, cloud_decoded, name):
     ax[-1].set_title("decoded cloud")
     plt.savefig(f"/content/pointnet.pytorch/{name}.png")
 
-"""
-def printCloud(cloud_original, cloud_decoded, name, alpha=0.5, opt=None):
+
+def printCloud(cloud_original, name, alpha=0.5, opt=None):
     alpha = 0.5
     xyz = cloud_original[0]
-    # print("sono qui")
+    print("sono qui")
     fig = plt.figure(figsize=(15, 15))
     ax = fig.add_subplot(111, projection='3d')
     ax.plot(xyz[:, 0], xyz[:, 1], xyz[:, 2], 'o', alpha=alpha)
     ax.set_title("original cloud")
-    xyz = cloud_decoded[0]
-    ax.append(fig.add_subplot(111, projection='3d'))
-    ax[-1].plot(cloud_decoded[:, 0], cloud_decoded[:, 1], cloud_decoded[:, 2], 'o', alpha=alpha)
-    ax[-1].set_title("decoded cloud")
     folder = "/content/pointnet.pytorch/images/" if opt is None else os.path.join(opt.outf, "images")
     try:
         os.makedirs(folder)
     except OSError:
         pass
     plt.savefig(os.path.join(folder, f"{hash(str(opt))}_{name}.png"))
-"""
-def printCloud(cloud_original, cloud_decoded, name, alpha=0.5, opt=None):
+
+def printCloudM(cloud_original, cloud_decoded, name, alpha=0.5, opt=None):
     alpha = 0.5
     xyz = cloud_original[0]
     print("sono qui")
@@ -49,7 +45,7 @@ def printCloud(cloud_original, cloud_decoded, name, alpha=0.5, opt=None):
     ax.set_title("original cloud")
     xyz = cloud_decoded[0]
     ax.append(fig.add_subplot(111, projection='3d'))
-    ax[-1].plot(cloud_decoded[:, 0], cloud_decoded[:, 1], cloud_decoded[:, 2], 'o', alpha=alpha)
+    ax[-1].plot(xyz[:, 0], xyz[:, 1], xyz[:, 2], 'o', alpha=alpha)
     ax[-1].set_title("decoded cloud")
     folder = "/content/pointnet.pytorch/images/" if opt is None else os.path.join(opt.outf, "images")
     try:
