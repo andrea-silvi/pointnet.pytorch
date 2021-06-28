395b4813acde22ce5c6d878fad5fdb8d4a416f77
Andrea Silvi <andrea.silvi@studenti.polito.it>
2021-06-28 18:48:57 +0200

    gcnn bozza

--- gcnn/gcnn_model.py
+++ gcnn/gcnn_model.py
@@ -117,7 +117,7 @@ class Decoder(nn.Module):
         self.bn1 = nn.BatchNorm1d(512)
         self.dp1 = nn.Dropout(p=args.dropout)
         self.linear2 = nn.Linear(512, 1024)
-        self.bn7 = nn.BatchNorm1d(1024)
+        self.bn2 = nn.BatchNorm1d(1024)
         self.dp2 = nn.Dropout(p=args.dropout)
         self.linear3 = nn.Linear(1024, 1024*3)
 
