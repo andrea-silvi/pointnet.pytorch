3977fa6ff2b58a2b5e825af544d7b4d11f9e0939
vitto <vittoriop.17@gmail.com>
2021-07-07 13:43:57 +0200

    decoder segmentation modified

--- point_completion/mutlitask_model.py
+++ point_completion/mutlitask_model.py
@@ -76,8 +76,6 @@ class PointNetDenseCls(nn.Module):
     def __init__(self, k = 2, feature_transform=False):
         super(PointNetDenseCls, self).__init__()
         self.k = k
-        self.feature_transform=feature_transform
-        self.feat = PointNetfeat(global_feat=False, feature_transform=feature_transform)
         self.conv1 = torch.nn.Conv1d(1088, 512, 1)
         self.conv2 = torch.nn.Conv1d(512, 256, 1)
         self.conv3 = torch.nn.Conv1d(256, 128, 1)
@@ -89,7 +87,6 @@ class PointNetDenseCls(nn.Module):
     def forward(self, x):
         batchsize = x.size()[0]
         n_pts = x.size()[2]
-        x, trans, trans_feat = self.feat(x)
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
@@ -97,7 +94,7 @@ class PointNetDenseCls(nn.Module):
         x = x.transpose(2,1).contiguous()
         x = F.log_softmax(x.view(-1,self.k), dim=-1)
         x = x.view(batchsize, n_pts, self.k)
-        return x, trans, trans_feat
+        return x
 
 
 class PointPyramidDecoder(nn.Module):
